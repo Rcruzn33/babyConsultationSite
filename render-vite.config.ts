772688551290cb4d44702c431b70f778e58dce13
Ruben <110207155+Rcruzn33@users.noreply.github.com>
2025-07-14 21:11:35 -0700

    Update render-vite.config.ts

--- render-vite.config.ts
+++ render-vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(process.cwd(), "client", "src"),
+      "@shared": path.resolve(process.cwd(), "shared"),
+      "@assets": path.resolve(process.cwd(), "attached_assets"),
     },
   },
   root: path.resolve(__dirname, "client"),
